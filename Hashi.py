f4969dcc3414f38955828ed39162a178fa6fb4f9
Jaymark <JaymarkAnit@gmail.com>
2025-11-30 20:12:14 +0800

    creating comment on matrices

--- Hashi.py
+++ Hashi.py
@@ -8,6 +8,8 @@ WINDOW_HEIGHT = 720
 screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption("Hashi Puzzle Game")
 
+# island 
+
 easy_matrix = [
     [2, 0, 0, 0, 0, 4, 0, 5, 0, 0, 4],  # row 0 
     [0, 4, 0, 4, 0, 0, 0, 0, 0, 1, 0],  # row 1
